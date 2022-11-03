9045aee6d1a447f8a0b95a13637e352ce31a82dc
Joshua Jerred <jdjerred@gmail.com>
2022-11-03 11:57:38 -0600

    test-04 added

---
+++ tests/test-04/test.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file test.cpp
+ * @author Joshua Jerred (github.com/joshua-jerred)
+ * @brief Tests for the Extension Module
+ * @date 2022-11-03
+ * @copyright Copyright (c) 2022
+ * @version 0.1.0
+ */
+
+#include <string>
+#include <chrono>
+#include <thread>
+
+#include "gtest/gtest.h"
+
+#include "utility-data-stream.h"
+#include "module-extensions.h"
+
+
+class ExtensionsModuleTests : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+        data_stream_ = new DataStream();
+    }
+    virtual void TearDown() {
+
+    }
+    DataStream *data_stream_;
+    ExtensionsModule *extensions_module_;
+};
+
+TEST_F(ExtensionsModuleTests, StartAndStop) {
+
+}
