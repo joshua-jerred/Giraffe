bc17ebc46d42ccefe4e41391b40271f49f3495a1
Joshua Jerred <jdjerred@gmail.com>
2023-05-10 00:19:47 -0600

    test structure change

---
+++ src/flight_software/tests/test_01/test.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file test.cpp
+ * @author Joshua Jerred (https://joshuajer.red)
+ * @brief Unit Tests for the Configuration Definition
+ * @date 2023-05-09
+ * @copyright Copyright (c) 2023
+ */
+
+#include <string>
+#include <time.h>
+
+#include "gtest/gtest.h"
+#include "yaml-cpp/yaml.h"
+class TEST_Configuration_Definition : public ::testing::Test {
+protected:
+    virtual void SetUp() { 
+     }
+    virtual void TearDown() { 
+    }
+};
+
+TEST_F(TEST_Configuration_Definition, Project_Name) {
+
+}
