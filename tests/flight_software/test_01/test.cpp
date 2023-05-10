64b6fece47cc81c881f012d494404f4c82940c9b
Joshua Jerred <jdjerred@gmail.com>
2023-05-09 20:53:26 -0600

    gfs unit tests update

---
+++ tests/flight_software/test_01/test.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file test.cpp
+ * @author Joshua Jerred (https://joshuajer.red/)
+ * @brief Data Stream utility tests
+ * 
+ * @version 0.1.0
+ * @date 2022-09-29
+ * @copyright Copyright (c) 2022
+ */
+
+#include <string>
+#include <time.h>
+
+#include "gtest/gtest.h"
+
+#include "data-stream.h"
+
+class TEST_Configuration : public ::testing::Test {
+protected:
+    virtual void SetUp() { 
+     }
+    virtual void TearDown() { 
+    }
+};
+
+TEST_F(TEST_Configuration, Constructor) {
+
+}
