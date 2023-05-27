45821eeb4d6ae029fd4722f7d4ea950064c41b3c
Joshua Jerred <jdjerred@gmail.com>
2023-05-26 20:23:24 -0500

    log ids generation

---
+++ src/flight_software/modules/data/log_ids.hpp
@@ -0,0 +1,24 @@
+// * * * * GENERATED AUTOMATICALLY, DO NOT MANUALLY MODIFY * * * *
+
+#ifndef LOG_IDS.HPP_HPP_
+
+enum class LogId {
+  GENERIC_unknown,
+  GENERIC_info,
+  GENERIC_debug,
+  MODULE_startFail,
+  CONFIG_sectionNotFound,
+  CONFIG_settingNotFound,
+  CONFIG_invalidJsonType,
+  CONFIG_invalidSettingValue,
+  CONFIG_enumNotString,
+  CONFIG_keyNotFoundInEnum,
+  CONFIG_failedToSaveToPath,
+  CONFIG_failedToOpenConfig,
+  CONFIG_configFileDoesNotExist,
+  CONFIG_failedToLoadSectionNotFound
+};
+
+#endif // LOG_IDS.HPP_HPP_
+
+// * * * * GENERATED AUTOMATICALLY, DO NOT MANUALLY MODIFY * * * *
