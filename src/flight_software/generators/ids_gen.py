044f987c0b323001dceaa0b797be643cba6c5999
Joshua Jerred <jdjerred@gmail.com>
2023-05-26 20:12:16 -0500

    data ids with generator

--- src/flight_software/generators/ids_gen.py
+++ src/flight_software/generators/ids_gen.py
@@ -1,60 +1,96 @@
 import sys
 import json
+import os
+
+import cpp_gen as cpp
 
 INDENT = "  "
 
-if (len(sys.argv) != 4 or sys.argv[3] not in ["data", "log"]):
-    print("data_ids_gen.py ARGS INVALID")
-    sys.exit(-1)
+IN_FILE = os.getenv('ID_GEN_IN_FILE_PATH')
+if (IN_FILE == None):
+    print("Error: ID_GEN_IN_FILE_PATH environment variable not set. Set from vscode tasks.json")
+    sys.exit(1)
+else:
+    print("IN_FILE: " + IN_FILE)
 
-mode = sys.argv[3] # 'data' or 'error'
+OUT_FILE = os.getenv('ID_GEN_OUT_FILE_PATH')
+if (OUT_FILE == None):
+    print("Error: ID_GEN_OUT_FILE_PATH environment variable not set. Set from vscode tasks.json")
+    sys.exit(1)
+else:
+    print("OUT_FILE: " + OUT_FILE)
 
-print("\nGenerating data_ids.hpp with mode: " + mode)
+PRINT_OUTPUT = True if os.getenv('PRINT_OUTPUT') == "true" else False
 
-file_top = """
-// * * * * GENERATED AUTOMATICALLY WITH ids_gen.py * * * *
-
-#ifndef {0}_IDS_HPP_
-#define {0}_IDS_HPP_
-
-namespace data {{
-enum class {1}Id {{
-""".format(mode.upper(), mode)
-
-
-file_bottom = """};
-}
-
-#endif"""
-
-in_data = open(sys.argv[1])
-json_data = json.load(in_data)
-
-data_item_list = []
-
-def processSection(prefix: str, section: dict):
-    for key in section:
-        if key == "ID":
-            continue
+class IdGenerator:
+    def __init__(self, in_file, out_file) -> None:
+        self.enum = None
         
-        data_item = section[key]
-        if "ID" not in data_item:
-            return
-        data_item_list.append(prefix + "_" + data_item["ID"])
-        print(" - " + data_item_list[-1])
-    
-for key in json_data:
-    section = json_data[key]
-    if "ID" not in section:
-        continue
-    processSection(section["ID"], json_data[key])
+        self.in_path = in_file
+        self.out_path = out_file
+        self.data = None
+        
+        self.namespace = ""
+        self.enum_name = ""
+        
+        self.__loadData()
+        self.__parse()
+        self.__write()
 
+    def __loadData(self):
+        f = open(self.in_path, "r")
+        self.data = json.load(f)
+        
+        self.namespace = self.data["FILE_META"]["namespace"]
+        self.enum_name = self.data["FILE_META"]["enum_name"]
+        self.enum = cpp.components.Enum(self.enum_name)
+        
+    def __parse(self):
+        for category_key in self.data:
+            if category_key == "FILE_META":
+                continue
+            for item in self.data[category_key]:
+                self.__addEnum(category_key, item, self.data[category_key][item])
 
-with open(sys.argv[2], "w") as out_file:
-    out_file.write(file_top)
-    for item in data_item_list:
-        item = INDENT + item + ",\n"
-        out_file.write(item)
-    out_file.write(file_bottom)
+    def __addEnum(self, category, item_name, json_data):
+        enum_name = ""
+        enum_name += category.upper() + "_"
+        
+        item_name_parts = item_name.split("_")
+        first = True
+        for part in item_name_parts:
+            if first:
+                first = False
+                enum_name += part.lower()
+            else:
+                enum_name += part.title()
+                
+        comment = None
+        if ("name" in json_data):
+            comment = json_data["name"]
+            if PRINT_OUTPUT:
+                print(f'{enum_name} {comment}')
+        else:
+            if PRINT_OUTPUT:
+                print(f'{enum_name}')
+
+        self.enum.addValue(enum_name, comment)
+
+    def __write(self):
+        file_name = OUT_FILE.split("/")[-1]
+        file = cpp.files.HppGenerator(file_name)
+        file.addComponent(self.enum)
+        print(file)
+        file.save(OUT_FILE)
+
+if __name__ == "__main__":
+    IdGenerator(IN_FILE, OUT_FILE)
+
+# with open(sys.argv[2], "w") as out_file:
+    # out_file.write(file_top)
+    # for item in data_item_list:
+        # item = INDENT + item + ",\n"
+        # out_file.write(item)
+    #out_file.write(file_bottom)
     
 print("Done.\n")
---
+++ src/flight_software/modules/data/data_ids.hpp
@@ -0,0 +1,36 @@
+// * * * * GENERATED AUTOMATICALLY, DO NOT MANUALLY MODIFY * * * *
+
+#ifndef DATA_IDS.HPP_HPP_
+
+enum class DataId {
+  GENERIC_unknown,
+  MODULES_STATUSES_data, // Data Module Status
+  MODULES_STATUSES_console,
+  MODULES_STATUSES_server,
+  MODULES_STATUSES_system,
+  SYSTEM_MODULE_cpu,
+  DATA_MODULE_dsProcessDelay, // Data Stream Processing Delay
+  DATA_MODULE_esPd, // Error Stream Processing Delay
+  DATA_MODULE_loggingDataToFile,
+  DATA_MODULE_loggingErrorsToFile,
+  SERVER_MODULE_numTotalPacketsRecv,
+  SERVER_MODULE_numTotalPacketsSent,
+  SERVER_MODULE_numFailedPackets,
+  SERVER_MODULE_clientAddress,
+  SERVER_MODULE_connectionStatus,
+  CONFIGURATION_configurationErrors,
+  STREAMS_dataStreamCurrent,
+  STREAMS_dataStreamTotal,
+  STREAMS_errorStreamCurrent,
+  STREAMS_errorStreamTotal,
+  CRITICAL_flightPhase, // Flight Phase
+  POSITION_GPS_lat,
+  POSITION_GPS_lon,
+  POSITION_IMU_xRoll,
+  POSITION_IMU_yRoll,
+  POSITION_IMU_zRoll
+};
+
+#endif // DATA_IDS.HPP_HPP_
+
+// * * * * GENERATED AUTOMATICALLY, DO NOT MANUALLY MODIFY * * * *
