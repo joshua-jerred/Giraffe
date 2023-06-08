48e9633375622d8fa5de8ec2254910388d5ce5ab
Joshua Jerred <jdjerred@gmail.com>
2023-06-07 22:56:00 -0500

    Shared Data Debug Log

--- src/flight_software/modules/data/shared_data.hpp
+++ src/flight_software/modules/data/shared_data.hpp
@@ -1,3 +1,20 @@
+/**
+ * =*========GIRAFFE========*=
+ * A Unified Flight Command and Control System
+ * https://github.com/joshua-jerred/Giraffe
+ * https://giraffe.joshuajer.red/
+ * =*=======================*=
+ *
+ * @file   shared_data.hpp
+ * @brief  Contains the SharedData struct, used across all modules and
+ * extensions.
+ *
+ * =*=======================*=
+ * @author     Joshua Jerred (https://joshuajer.red)
+ * @date       2023-06-07
+ * @copyright  2023 (license to be defined)
+ */
+
 #ifndef SHARED_DATA_HPP_
 #define SHARED_DATA_HPP_
 
@@ -10,7 +27,6 @@
 #include <BoosterSeat/time.hpp>
 
 namespace data {
-
 struct Streams {
   DataStream data = DataStream();
   LogStream log = LogStream();
@@ -47,6 +63,28 @@ struct SharedData {
   Frames frames = Frames();
   Blocks blocks = Blocks();
   Misc misc = Misc();
+
+#ifndef DNDEBUG // Defined by CMake
+  /**
+   * @brief Used to log debug messages when the program is not in release mode.
+   *
+   * @param id - Node identification
+   * @param info - Debug message
+   */
+  void debugLog(const node::Identification id, const std::string &info) {
+    streams.log.debug(id, info);
+  }
+#else
+  /**
+   * @brief This function does nothing when the program is in release mode.
+   * @param id
+   * @param info
+   */
+  void debugLog(const node::Identification id, const std::string &info) {
+    (void)id;
+    (void)info;
+  }
+#endif
 };
 
 } // namespace data
