282baa913703828b1dba6b7ebb38ae0eb76f193e
Joshua Jerred <jdjerred@gmail.com>
2023-06-12 21:07:55 -0500

    CMake and meta loading update

--- sources.cmake
+++ sources.cmake
@@ -1,17 +1,38 @@
+
+# --- DIRECTORIES --- #
+set(COMMON_SRC ${CMAKE_SOURCE_DIR}/src/common)
+set(COMMON_BIN ${CMAKE_BINARY_DIR}/common)
+set(COMMON_TEST_SRC ${CMAKE_SOURCE_DIR}/tests/common)
+set(COMMON_TEST_BIN ${CMAKE_BINARY_DIR}/tests/common)
+
+set(GFS_SRC  ${CMAKE_SOURCE_DIR}/src/flight_system)
+set(GFS_BIN ${CMAKE_BINARY_DIR}/flight_system)
+set(GFS_TEST_SRC ${CMAKE_SOURCE_DIR}/tests/flight_system)
+set(GFS_TEST_BIN ${CMAKE_BINARY_DIR}/bin/tests/flight_system)
+
+set(LIB_SRC ${CMAKE_SOURCE_DIR}/lib)
+# ------------------- #
+
 set(common_sources
 	${COMMON_SRC}/protocol/protocol.cpp
 )
 
-set(common_includes
-	${COMMON_SRC}/
+set(common_include_dirs
+	${COMMON_SRC}
+	${COMMON_SRC}/protocol
 )
 
 set(gfs_sources
+	# common
+	${common_sources}
+
 	# main
+	${GFS_SRC}/main.cpp
 	${GFS_SRC}/flight_runner.cpp
 
 	# configuration 
 	${GFS_SRC}/configuration/configuration.cpp
+	${GFS_SRC}/configuration/sections/cfg_extensions.cpp
 
 	# module base
 	${GFS_SRC}/modules/module.cpp
@@ -34,9 +55,13 @@ set(gfs_sources
 
 	# system module
 	${GFS_SRC}/modules/system/system_module.cpp
+
+	# extensions module and extensions
+	${GFS_SRC}/modules/extensions/extension_module.cpp
 )
 
 set(gfs_include_dirs
+	${common_include_dirs}
 	${GFS_SRC}/
 	${GFS_SRC}/utilities
 	${GFS_SRC}/configuration
@@ -45,4 +70,5 @@ set(gfs_include_dirs
 	${GFS_SRC}/modules/console
 	${GFS_SRC}/modules/server
 	${GFS_SRC}/modules/system
+	${GFS_SRC}/modules/extensions
 )
