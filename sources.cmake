4b94eb13ba7a0f8d7b8051f43f7a641d2ca588de
Joshua Jerred <jdjerred@gmail.com>
2023-06-23 22:25:48 -0500

    build paths change

--- sources.cmake
+++ sources.cmake
@@ -3,10 +3,10 @@
 set(COMMON_SRC ${CMAKE_SOURCE_DIR}/src/common)
 set(COMMON_BIN ${CMAKE_BINARY_DIR}/common)
 set(COMMON_TEST_SRC ${CMAKE_SOURCE_DIR}/tests/common)
-set(COMMON_TEST_BIN ${CMAKE_BINARY_DIR}/tests/common)
+set(COMMON_TEST_BIN ${CMAKE_BINARY_DIR}/bin/tests/common)
 
 set(GFS_SRC  ${CMAKE_SOURCE_DIR}/src/flight_system)
-set(GFS_BIN ${CMAKE_BINARY_DIR}/flight_system)
+set(GFS_BIN ${CMAKE_BINARY_DIR}/bin/flight_system)
 set(GFS_TEST_SRC ${CMAKE_SOURCE_DIR}/tests/flight_system)
 set(GFS_TEST_BIN ${CMAKE_BINARY_DIR}/bin/tests/flight_system)
 
