1b406fe24669764f6487bba1d022fab06242d3df
Joshua Jerred <contact@joshuajer.red>
2024-01-18 23:36:07 -0700

    gdl - pulse audio fixes

--- src/data_link/include/gdl_constants.hpp
+++ src/data_link/include/gdl_constants.hpp
@@ -21,14 +21,17 @@
 #include <cstdlib>
 
 namespace giraffe::gdl {
-inline constexpr static int GDL_THREAD_SLEEP_INTERVAL_MS = 0;
+inline constexpr static int GDL_THREAD_SLEEP_INTERVAL_MS = 200;
 
 inline constexpr static size_t GDL_MESSAGE_DATA_MAX_SIZE = 150;
 
 inline constexpr static size_t GDL_MESSAGE_QUEUES_SIZE = 10;
 
-inline constexpr static uint8_t GDL_RDT_MAX_RETRIES = 3;
-inline constexpr static uint32_t GDL_RDT_RETRY_INTERVAL_MS = 500;
+/// @brief delay between sending a new transmission
+inline constexpr static uint32_t GDL_NEW_TX_DELAY_MS = 100;
+
+inline constexpr static uint8_t GDL_RDT_MAX_RETRIES = 5;
+inline constexpr static uint32_t GDL_RDT_RETRY_INTERVAL_MS = 5000;
 inline constexpr static uint32_t GDL_CONNECTION_STATUS_INTERVAL_MS = 120 * 1000;
 
 inline constexpr static uint32_t GDL_EXCHANGE_CONNECTION_TIMEOUT_MS = 30 * 1000;
