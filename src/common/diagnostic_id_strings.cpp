0de0b08a4cd2697591f068b0388fc19d160e0044
Joshua Jerred <contact@joshuajer.red>
2024-01-24 04:59:53 -0700

    no gps source warning

--- src/common/diagnostic_id_strings.cpp
+++ src/common/diagnostic_id_strings.cpp
@@ -55,6 +55,7 @@ const std::unordered_map<DiagnosticId, std::string> K_DIAGNOSTIC_ID_STRING_MAP {
   {DiagnosticId::DATA_MODULE_cameraNewImagePacketInvalidPath,"DATA_MODULE_cameraNewImagePacketInvalidPath"},
   {DiagnosticId::DATA_MODULE_cameraNewImagePacketInvalidFields,"DATA_MODULE_cameraNewImagePacketInvalidFields"},
   {DiagnosticId::DATA_MODULE_unrecognizedCommand,"DATA_MODULE_unrecognizedCommand"},
+  {DiagnosticId::DATA_MODULE_noGpsSource,"DATA_MODULE_noGpsSource"},
   {DiagnosticId::DATA_LOG_invalidDataframeStrategy,"DATA_LOG_invalidDataframeStrategy"},
   {DiagnosticId::DATA_LOG_archiveOldDataFiles,"DATA_LOG_archiveOldDataFiles"},
   {DiagnosticId::DATA_LOG_archiveOldLogFiles,"DATA_LOG_archiveOldLogFiles"},
