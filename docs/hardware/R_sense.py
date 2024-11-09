d1a307daa04a60647db63f0156124a2258108377
Joshua Jerred <19292194+joshua-jerred@users.noreply.github.com>
2024-11-09 15:08:13 -0600

    5v buck supply bom and r sense

--- docs/hardware/R_sense.py
+++ docs/hardware/R_sense.py
@@ -1,19 +1,20 @@
 # Configurable
-MAX_CURRENT = 2.0 # A (I_MAX)
-MAX_POWER_DISSIPATION = 0.5 # W (PD_MAX)
-VOLTAGE = 5.0 # V
-GAIN = 20.0 # (2O/50/100/200 depending on device)
+MAX_CURRENT = 2.5 # A (I_MAX)
+MIN_CURRENT = 0.3 # A (I_MIN)
+MIN_VOLTAGE = 6.0 # V
+MAX_VOLTAGE = 8.4 # v
 
-R_SENSE = 1.12
+R_SENSE = 0.02 # Ohm
+GAIN = 50.0 # (2O/50/100/200 depending on device)
 
-# Constants
-V_SP = -0.02 # Max swing to Vs power-supply rail (typical, max is -0.03)
+max_vdrop = MAX_CURRENT * R_SENSE
+min_vdrop = MIN_CURRENT * R_SENSE
+min_vdrop_amplified = min_vdrop * GAIN
+max_vdrop_amplified = max_vdrop * GAIN
 
-r_sense_lt = MAX_POWER_DISSIPATION / (MAX_CURRENT**2)
-print("R_sense < {:.2f} Ohm".format(r_sense_lt))
+max_power_dissipation = MAX_CURRENT * MAX_CURRENT * R_SENSE
 
-if (R_SENSE >= r_sense_lt):
-  print("Invalid R_SENSE")
-
-v_sp_lt = MAX_CURRENT * MAX_POWER_DISSIPATION * GAIN
-print(f'{v_sp_lt} < {V_SP}')
+print("Voltage drop [min, max]:", min_vdrop, max_vdrop) # This should be low
+print("Voltage drop amplified [min, max]:", min_vdrop_amplified, max_vdrop_amplified) # This should be in the range of the ADC (0-3.3V)
+print("Min output voltage:", MIN_VOLTAGE - max_vdrop) # This should be higher than the dropout voltage of the power supply
+print(f"Max power dissipation: {max_power_dissipation}W") # This should be low
