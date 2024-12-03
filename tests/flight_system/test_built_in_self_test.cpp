1c9e8762e0332997e6cb59483383771025c32832
Joshua Jerred <19292194+joshua-jerred@users.noreply.github.com>
2024-12-03 01:18:54 -0600

    fixed unit test

--- tests/flight_system/test_built_in_self_test.cpp
+++ tests/flight_system/test_built_in_self_test.cpp
@@ -11,9 +11,20 @@
 /// @date       2024-11-16
 /// @copyright  2024 (license to be defined)
 
-#include "test_case.hpp"
+#include "test_cases.hpp"
 #include "unit_test.hpp"
 
-TEST(BitTest_TestCase, initialState) {
-  bit::TestCase test_case{bit::TestGroupId::FCS, bit::TestId::BATT_AdcTest};
+TEST(BIT_TestCase, FCS_0001) {
+  data::SharedData sharedData;
+  auto results = bit::test_case::runTestFCS_0001(sharedData);
+  EXPECT_EQ(results.status, bit::TestStatus::PASSED);
+
+  // Add an error to the error frame
+  data::LogPacket errorPacket{};
+  errorPacket.id = DiagnosticId::CONFIG_adcMappingLabelAlreadyExists;
+  errorPacket.level = data::LogPacket::Level::ERROR;
+  sharedData.frames.error_frame.addError(errorPacket);
+
+  results = bit::test_case::runTestFCS_0001(sharedData);
+  EXPECT_EQ(results.status, bit::TestStatus::FAILED);
 }
